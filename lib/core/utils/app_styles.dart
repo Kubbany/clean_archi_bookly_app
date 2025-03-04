4658ea5ed0824755f6c55651798dadc678d2e65b
Kubbany <2021170289@cis.asu.edu.eg>
2025-03-04 21:00:56 +0200

    styles created

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -2,26 +2,26 @@ import 'package:bookly_app/constants.dart';
 import 'package:flutter/cupertino.dart';
 
 abstract class AppStyles {
-  static const textStyle18 = TextStyle(
+  static const styleSemiBold18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
   );
-  static const textStyle20 = TextStyle(
+  static const styleNormal20 = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.normal,
   );
-  static const textStyle30 = TextStyle(
+  static const styleThick30 = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.w900,
     fontFamily: kGtSectraFine,
     letterSpacing: 1.2,
   );
-  static const textStyle14 = TextStyle(
+  static const styleNormal14 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.normal,
   );
 
-  static const textStyle16 = TextStyle(
+  static const styleMedium16 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w500,
   );
