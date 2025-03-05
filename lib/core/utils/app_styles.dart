08981911d78e5b7bb323752c9c5b8967d98ec54d
Kubbany <2021170289@cis.asu.edu.eg>
2025-03-05 17:43:14 +0200

    add book details view

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -1,29 +1,34 @@
 import 'package:bookly_app/constants.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 abstract class AppStyles {
   static const styleSemiBold18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
+    color: Colors.white,
   );
   static const styleNormal20 = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.normal,
+    color: Colors.white,
   );
   static const styleThick30 = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.w900,
     fontFamily: kGtSectraFine,
     letterSpacing: 1.2,
+    color: Colors.white,
   );
   static const styleNormal14 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.normal,
+    color: Colors.white,
   );
 
   static const styleMedium16 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w500,
+    color: Colors.white,
   );
 }
 
