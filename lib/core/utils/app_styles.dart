7beaa1f70418b9ca28cc151944680659754e0d3e
Kubbany <2021170289@cis.asu.edu.eg>
2025-03-05 15:39:30 +0200

    best seller book item done

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -8,6 +8,7 @@ abstract class AppStyles {
   );
   static const styleNormal20 = TextStyle(
     fontSize: 20,
+    fontFamily: kGtSectraFine,
     fontWeight: FontWeight.normal,
   );
   static const styleThick30 = TextStyle(
--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/app_styles.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_image.dart';
 import 'package:flutter/material.dart';
@@ -7,15 +8,29 @@ class BestSellerListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
+      spacing: 30,
       children: <Widget>[
-        SizedBox(
+        const SizedBox(
           height: 150,
           child: CustomImage(
             aspectRatio: 0.6,
             image: AppAssets.testImage,
             borderRadius: 8,
           ),
+        ),
+        Column(
+          children: <Widget>[
+            SizedBox(
+              width: MediaQuery.sizeOf(context).width * 0.5,
+              child: const Text(
+                "Harry Potter and The Goblet of Fire",
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: AppStyles.styleNormal20,
+              ),
+            ),
+          ],
         ),
       ],
     );
