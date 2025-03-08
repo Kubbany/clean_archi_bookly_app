fc242c08d87029f0c66efab64b188b479dfa60bb
Kubbany <2021170289@cis.asu.edu.eg>
2025-03-08 17:56:11 +0200

    changing pars use case name

--- lib/core/use_cases/pars_use_case.dart
+++ lib/core/use_cases/pars_use_case.dart
@@ -1,6 +1,6 @@
 import 'package:bookly_app/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class UseCase<T, P> {
+abstract class ParsUseCase<T, P> {
   Future<Either<Failure, T>> execute([P pars]);
 }
