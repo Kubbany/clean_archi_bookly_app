1c14098c9a186d3cfaaadddc265e203b946b45f0
Kubbany <2021170289@cis.asu.edu.eg>
2025-03-08 15:25:44 +0200

    add book entity

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,12 @@
+class BookEntity {
+  final String image, title, authorName;
+  final num price, rating;
+
+  BookEntity({
+    required this.image,
+    required this.title,
+    required this.authorName,
+    required this.price,
+    required this.rating,
+  });
+}
