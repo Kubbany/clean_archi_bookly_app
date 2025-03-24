cfb2f169d5d34358fb06286efb7559d19cd8cd5b
Kubbany <2021170289@cis.asu.edu.eg>
2025-03-24 17:01:24 +0200

    Skeletonizing image item

--- lib/features/home/presentation/views/widgets/custom_image.dart
+++ lib/features/home/presentation/views/widgets/custom_image.dart
@@ -1,5 +1,7 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class CustomImage extends StatelessWidget {
   const CustomImage({
@@ -19,8 +21,12 @@ class CustomImage extends StatelessWidget {
           Radius.circular(16),
         ),
         child: CachedNetworkImage(
+          placeholder: (context, url) => Skeletonizer(
+            child: Image.asset(AppAssets.testImage),
+          ),
           errorWidget: (context, url, error) => Container(
             color: Colors.grey,
+            child: const Icon(Icons.error_outline),
           ),
           imageUrl: image,
           fit: BoxFit.fill,
